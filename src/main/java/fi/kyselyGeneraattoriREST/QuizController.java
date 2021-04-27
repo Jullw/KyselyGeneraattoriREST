da4ae39f17199aca4f021980f324d7a13021a247
Jullw <j.sajaniemi@gmail.com>
2021-04-27 13:54:18 +0300

    CrossOrigin lisatty

--- src/main/java/fi/kyselyGeneraattoriREST/QuizController.java
+++ src/main/java/fi/kyselyGeneraattoriREST/QuizController.java
@@ -1,7 +1,7 @@
 package fi.kyselyGeneraattoriREST;
 
 import java.util.List;
-
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -14,28 +14,33 @@ import org.springframework.web.bind.annotation.RestController;
 class QuizController {
 
     private final QuizRepository repository;
-
+    
+    
     QuizController(QuizRepository repository) {
         this.repository = repository;
     }
-
+    
+    @CrossOrigin
     @GetMapping("/quizzes")
     List<Quiz> all() {
         return repository.findAll();
     }
-
+    
+    @CrossOrigin
     @PostMapping("/quizzes")
     Quiz newQuiz(@RequestBody Quiz newQuiz) {
         return repository.save(newQuiz);
     }
-
+    
+    @CrossOrigin
     @GetMapping("/quizzes/{id}")
     Quiz one(@PathVariable Long id) {
 
         return repository.findById(id)
                 .orElseThrow(() -> new QuizNotFoundException(id));
     }
-
+    
+    @CrossOrigin
     @PutMapping("/quizzes/{id}")
     Quiz replaceQuiz(@RequestBody Quiz newQuiz, @PathVariable Long id) {
 
@@ -50,7 +55,8 @@ class QuizController {
                     return repository.save(newQuiz);
                 });
     }
-
+    
+    @CrossOrigin
     @DeleteMapping("/quizzes/{id}")
     void deleteQuiz(@PathVariable Long id) {
         repository.deleteById(id);
